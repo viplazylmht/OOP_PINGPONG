976b5bd59533d45d5af265cd81fcce4ceb1f7a4b
viplazylmht <viplazylmht@gmail.com>
2019-10-09 12:41:14 +0700

    Hotfix for SDL Lib

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 using namespace std;
 int main() {
+
+	//Initializes  the subsystems
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		printf("Unable to initialize SDL %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Window* window = NULL;
+	window = SDL_CreateWindow("PING PONG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		printf("Could not create window %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Delay(5000);
+
+	//Destroy a window
+	SDL_DestroyWindow(window);
+	//This function cleans up all initialized subsystems
+	SDL_Quit();
 
 	cout << "Hello World!\n";
 	return 0;
