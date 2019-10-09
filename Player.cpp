bc533323a4628c6a574f490f17e642fa38e5052a
BHD233 <buihuuduc233@gmail.com>
2019-10-09 21:06:40 +0700

    add draw and fix some thing
    what's new:
    + add draw and hide (for move)
    + add default value (for 1280x720)
    + add constructor with just pos and id
    + move function now attually move object by draw and hide

--- Player.cpp
+++ Player.cpp
@@ -2,34 +2,80 @@
 
 
 //Description: change position of x, and y base on give char
+//amd draw player base on new position (hide the old and draw the new one)
 //dir: 'l' for move left, 'r' for move right
-void Player::Move(char dir)
+//render: render for draw and hide
+void Player::Move(char dir, SDL_Renderer* render)
 {
-	if (dir == 'l')
+	//hide old one
+	Hide(render);
+
+	//calcualte new position
+	if (dir == 'u')
 	{
-		_pos.x -= _speed;
+		_pos.y -= _speed;
 	}
-	else if (dir == 'r')
+	else if (dir == 'd')
 	{
-		_pos.x += _speed;
+		_pos.y += _speed;
 	}
+
+	//draw new one
+	Draw(render);
 }
 
 //Description: show player to the board
-void Player::Draw()
+void Player::Draw(SDL_Renderer* render)
 {
 	//do something
+	SDL_Rect rect;
+	rect.x = _pos.x;
+	rect.y = _pos.y;
+	rect.h = _length;
+	rect.w = _width;
+
+	SDL_SetRenderDrawColor(render, 0, 0, 255, 255);
+
+	SDL_RenderFillRect(render, &rect);
+
+	SDL_RenderPresent(render);
 }
 
+//Description: hide player
+void Player::Hide(SDL_Renderer* render)
+{
+	SDL_Rect rect;
+	rect.x = _pos.x;
+	rect.y = _pos.y;
+	rect.h = _length;
+	rect.w = _width;
 
+	//draw black rectangle
+	SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
+
+	SDL_RenderFillRect(render, &rect);
+
+	SDL_RenderPresent(render);
+}
+
+//Description: create a player with pos (0, 0) and all default value (can use for 1280x720)
 Player::Player()
 {
 	_pos.x = 0;
 	_pos.y = 0;
-	_length = 0;
-	_width = 0;
-	_speed = 0;
+	_length = DEFAULTLENGTH;
+	_width = DEFAULTWIDTH;
+	_speed = DEFAULTSPEED;
 	_id = 0;
+}
+
+Player::Player(Point pos, int id)
+{
+	_pos = pos;
+	_length = DEFAULTLENGTH;
+	_width = DEFAULTWIDTH;
+	_speed = DEFAULTSPEED;
+	_id = id;
 }
 
 Player::Player(Point pos, int length, int width, int speed, int id)
