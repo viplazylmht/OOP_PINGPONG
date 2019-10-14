2a3a0e882da84267512d93a34770843299b3b138
BHD233 <32787292+BHD233@users.noreply.github.com>
2019-10-14 20:57:04 +0700

    add hard CPU and fix some thing (#17)

--- CPU.cpp
+++ CPU.cpp
@@ -1,23 +1,67 @@
 #include "CPU.h"
+#include <math.h>
+#include <iostream>
+
+using namespace std;
 
 //Description: calculate direction of player base on ball player
-//height: acttual height of screen
-char CPU::CalcDirection(Ball ball, Player player, int height)
+//top: top of screen
+//bottom: bottom of screen
+//player: player that CPU control
+//Return: direction (up or down) or 0 if want CPU stand still
+char CPU::EasyCalcDirection(Ball ball, Player player, int top, int bottom)
 {
 	int centerOfPlayer = player.Pos().y + player.Length() / 2;
 	if (ball.Center().y < centerOfPlayer - _error
-		&& player.Pos().y > 0)
+		&& player.Pos().y > top)
 	{
-		return 'u';
+		return Player::MOVE_UP;
 	}
 	else if (ball.Center().y > centerOfPlayer + _error
-		&& player.Pos().y + player.Length() < height)
+		&& player.Pos().y + player.Length() < bottom)
 	{
-		return 'd';
+		return Player::MOVE_DOWN;
 	}
 	else
 	{
 		return 0;
+	}
+}
+
+//Description: calculate direction of player base on ball player
+//top: top of screen
+//bottom: bottom of screen
+//realWidth: acttual width of screen
+//player: player that CPU control
+//Return: direction (up or down) or 0 if want CPU stand still
+int CPU::HardCalcDestination(Ball ball, Player player, int top, int bottom, int realWidth)
+{
+	//let ball move
+	while (ball.Center().x + ball.Radius() < realWidth)
+	{
+		//collide wall
+		if (ball.Center().y <= ball.Radius() + top) {
+			ball.Collide(Ball::BORDER_TOP);
+		}
+		if (ball.Center().y >= bottom - ball.Radius()) {
+			ball.Collide(Ball::BORDER_BOTTOM);
+		}
+		ball.Move();
+	}
+
+	return ball.Center().y;
+}
+
+//Description: make player move to destination
+void CPU::MoveToDest(Player &player, int dest, int top, int bottom)
+{
+	if ((player.Pos().y + player.Length() / 2 < dest) && player.Pos().y < bottom)
+	{
+		player.Move(Player::MOVE_DOWN);
+	}
+	if ((player.Pos().y + player.Length() / 2 > dest) && player.Pos().y > top)
+	{
+		player.Move(Player::MOVE_UP);
 	}
 }
 
